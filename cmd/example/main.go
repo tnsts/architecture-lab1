ca5c24c76ea48cdb5a052a5061799ee9c465335b
tnsts <t.steblevets@gmail.com>
2020-02-20 18:48:36 +0200

    main.go implementation

---
+++ cmd/example/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	lab1 "https://github.com/tnsts/architecture-lab1"
+)
+
+func main() {
+	var expression string
+	for _, element := range os.Args[1:] {
+		expression += element + " "
+	}
+
+	res, err := lab1.CalculatePostfix(expression)
+	if err == nil {
+		fmt.Println(res)
+	} else {
+		fmt.Println("Error!", err)
+	}
+}
